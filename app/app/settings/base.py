d3162560b5f13cbb148c776735117da6624cfe46
Bogdan <mail@mail.co>
2026-05-28 16:42:53 +0300

    add OpenAPI schema and swagger documentation

--- app/app/settings/base.py
+++ app/app/settings/base.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'drf_spectacular',
 
     'departments',
     'employees'
@@ -59,6 +60,7 @@ REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
     ],
+     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
 LANGUAGE_CODE = "en-us"
@@ -85,3 +87,8 @@ LOGGING = {
     "disable_existing_loggers": True,
 }
 
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Organization API",
+    "DESCRIPTION": "Departments and Employees API",
+    "VERSION": "1.0.0",
+}
